26e3bd386189f5da69bd36f3b4729baab1806ad4
gaojian <A24619@autelsz.com>
2025-02-14 11:20:04 +0800

    fix bug

--- main/utils/mongo.py
+++ main/utils/mongo.py
@@ -15,9 +15,11 @@ class MongoClient:
         print(document)
         return document
 
-    async def find(self, query=None, collection_name="Data"):
+    async def find(self, query=None, collection_name="Data", sort=None):
         collection = self.db[collection_name]
         cursor = collection.find(query)
+        if sort:
+            cursor = cursor.sort(sort)
         documents = []
         async for document in cursor:
             documents.append(document)
@@ -30,35 +32,42 @@ class MongoClient:
         documents = await self.find(query, collection_name)
         return documents
 
-    async def find_by_week(self, week_start_date: str, env: str = "", collection_name: str = "Data"):
+    async def find_by_week(self,
+                           week_start_date: str,
+                           env: str = "",
+                           collection_name: str = "Data"):
         query = {"custom.week_start_date": week_start_date}
         if env:
             query["custom.env"] = env
         documents = await self.find(query, collection_name)
         return documents
 
-    async def find_by_range(self, start_date: str, end_date: str, env: str = "", collection_name: str = "Data"):
+    async def find_by_range(self,
+                            start_date: str,
+                            end_date: str,
+                            env: str = "",
+                            collection_name: str = "Data"):
         query = {"custom.date": {"$gte": start_date, "$lte": end_date}}
         if env:
             query["custom.env"] = env
         documents = await self.find(query, collection_name)
         return documents
 
-    async def find_errors(self, env:str="", date:str="", collection_name: str = "Data"):
+    async def find_errors(self, env: str = "", date: str = "", collection_name: str = "Data"):
         query = {
-            "evaluation.message_evaluation.693ce67c-98b9-4182-8a85-b1beb1aeda94": {"$exists": False}
+            "evaluation.message_evaluation.693ce67c-98b9-4182-8a85-b1beb1aeda94": {
+                "$exists": False
+            }
         }
         if env:
             query["custom.env"] = env
         if date:
             query["custom.date"] = date
-        documents = await self.find(query, collection_name)
+        documents = await self.find(query, collection_name, sort=[("custom.start_time", -1)])
         return documents
 
 
-
 mongo_client = MongoClient(uri="mongodb://admin:123456@10.240.3.251:27017", db_name="label_llm")
-
 
 if __name__ == "__main__":
     query = {"evaluation.message_evaluation.intent": "SUCCESS"}
